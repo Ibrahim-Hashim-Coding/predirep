b01500d9e8e983c41f5180c708a306fca810b50d
Ibrahim Hashim <65871176+Ibrahim-Hashim-Coding@users.noreply.github.com>
2024-01-25 16:56:45 +0100

    Update data_settings.py

--- data_settings.py
+++ data_settings.py
@@ -1,6 +1,5 @@
 # Where data is stored - direct path
-DATA_DIR = "/media/ibrahim/DataIbrahim/data/kitti/kitti_data/prednet_kitti_data/"
-#DATA_DIR = "./data/"
+DATA_DIR = "./data/"
 
 # Where model is saved - direct path
 WEIGHTS_DIR = "./weights/"
